39bdecd571f9d1f353865d356a05e1ee495084d2
william kreuch <williamkreuch@gmail.com>
2023-11-21 22:24:50 -0300

    Change project name

--- API_DJ_Managment/src/main/java/br/com/wkreuch/ApiDjManagementApplication.java
+++ API_DJ_Managment/src/main/java/br/com/wkreuch/ApiDjManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiDjManagmentApplication {
+public class ApiDjManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiDjManagmentApplication.class, args);
+		SpringApplication.run(ApiDjManagementApplication.class, args);
 	}
 
 }
