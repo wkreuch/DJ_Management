8f17407f4b13632bec7146eec3dd9dd9bb51eb15
william kreuch <williamkreuch@gmail.com>
2023-11-21 22:01:02 -0300

    initial files spring boot

---
+++ API_DJ_Managment/src/main/java/br/com/wkreuch/ApiDjManagmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.wkreuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDjManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDjManagmentApplication.class, args);
+	}
+
+}
---
+++ API_DJ_Managment/src/test/java/br/com/wkreuch/ApiDjManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wkreuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDjManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
