3e209550d9127f0bc443b6fb633aa68f942f7d49
william kreuch <williamkreuch@gmail.com>
2023-11-28 21:02:12 -0300

    add swagger in DJController

--- API_DJ_Managment/src/main/java/br/com/wkreuch/controllers/DJController.java
+++ API_DJ_Managment/src/main/java/br/com/wkreuch/controllers/DJController.java
@@ -5,6 +5,13 @@ import br.com.wkreuch.data.dtos.DJResponseDto;
 import br.com.wkreuch.models.DJ;
 import br.com.wkreuch.services.DJService;
 import br.com.wkreuch.utils.mapper.DjMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,34 +22,77 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/dj")
+@Tag(name = "DJ", description = "Endpoints for Managements DJs")
 public class DJController {
 
     @Autowired
     private DJService service;
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Adds a full DJ", description = "Adds a new DJ by passing in a JSON", tags = {"DJ"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "201", content = @Content(schema = @Schema(implementation = DJResponseDto.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public DJResponseDto create(@RequestBody @Valid DJCreateDto djCreateDto) {
         DJ newDj = DjMapper.convert(djCreateDto);
         return DjMapper.convert(service.create(newDj), true);
     }
 
     @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Updates a full DJ", description = "Updates a DJ by passing in a JSON", tags = {"DJ"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = DJResponseDto.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public DJResponseDto update(@PathVariable(value = "id") Long id, @RequestBody @Valid DJCreateDto djCreateDto) {
         DJ updateDj = DjMapper.convert(djCreateDto);
         return DjMapper.convert(service.update(id, updateDj),true);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Finds all DJs", description = "Finds all DJs", tags = {"DJ"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = DJResponseDto.class)))),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public List<DJResponseDto> findAll() {
         return DjMapper.convert(service.findAll(),true);
     }
 
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Finds a DJ", description = "Finds one DJ", tags = {"DJ"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = DJResponseDto.class))),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public DJResponseDto findById(@PathVariable(value = "id") Long id) {
         return DjMapper.convert(service.findById(id),true);
     }
 
     @DeleteMapping(path = "/{id}")
+    @Operation(summary = "Deletes a DJ", description = "Deletes one DJ", tags = {"DJ"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
+
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
