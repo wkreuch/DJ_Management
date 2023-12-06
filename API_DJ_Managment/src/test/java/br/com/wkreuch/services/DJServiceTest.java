9d3e4bea372c0e063ac78861bf650a4bae744092
william kreuch <williamkreuch@gmail.com>
2023-12-05 21:45:29 -0300

    add unit test DJServiceTest all methods

--- API_DJ_Managment/src/test/java/br/com/wkreuch/services/DJServiceTest.java
+++ API_DJ_Managment/src/test/java/br/com/wkreuch/services/DJServiceTest.java
@@ -1,5 +1,8 @@
 package br.com.wkreuch.services;
 
+import br.com.wkreuch.exceptions.ErrorCode;
+import br.com.wkreuch.exceptions.RequiredObjectIsNullException;
+import br.com.wkreuch.exceptions.ResourceNotFoundException;
 import br.com.wkreuch.models.DJ;
 import br.com.wkreuch.models.mock.DJMock;
 import br.com.wkreuch.repositories.DJRepository;
@@ -13,9 +16,9 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -63,5 +66,106 @@ public class DJServiceTest {
         assertEquals("South Carolina", result.getAddress().getState());
         assertEquals("United States", result.getAddress().getCountry());
         assertEquals("997720868", result.getAddress().getPostalCode());
+    }
+
+    @Test
+    void testUpdate() {
+        DJ dj = input.mockDJ(22L);
+        DJ djPersisted = input.mockDJ(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(djPersisted));
+        when(repository.save(djPersisted)).thenReturn(djPersisted);
+
+        var result = service.update(1L, dj);
+
+        assertNotNull(result);
+        assertNotNull(result.getIdDj());
+
+        assertEquals(1L, result.getIdDj());
+        assertEquals("Pierre David 22", result.getFirstName());
+        assertEquals("Guetta 22", result.getLastName());
+        assertEquals(LocalDate.parse("1967-10-07"), result.getBirthDate());
+        assertEquals("David Guetta", result.getArtistName());
+        assertEquals("123456", result.getCountryIdRegistration());
+
+        assertEquals("Elliot Courts 22", result.getAddress().getStreet());
+        assertEquals(634L, result.getAddress().getNumber());
+        assertEquals("Apt. 492", result.getAddress().getComplement());
+        assertEquals("Central", result.getAddress().getDistrict());
+        assertEquals("Kameronmouth", result.getAddress().getCity());
+        assertEquals("South Carolina", result.getAddress().getState());
+        assertEquals("United States", result.getAddress().getCountry());
+        assertEquals("997720868", result.getAddress().getPostalCode());
+    }
+
+    @Test
+    void testFindByID() {
+        DJ djPersisted = input.mockDJ(1L);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(djPersisted));
+
+        var result = service.findById(1L);
+
+        assertNotNull(result);
+        assertNotNull(result.getIdDj());
+
+        assertEquals(1L, result.getIdDj());
+        assertEquals("Pierre David 1", result.getFirstName());
+        assertEquals("Guetta 1", result.getLastName());
+        assertEquals(LocalDate.parse("1967-10-07"), result.getBirthDate());
+        assertEquals("David Guetta", result.getArtistName());
+        assertEquals("123456", result.getCountryIdRegistration());
+
+        assertEquals("Elliot Courts 1", result.getAddress().getStreet());
+        assertEquals(634L, result.getAddress().getNumber());
+        assertEquals("Apt. 492", result.getAddress().getComplement());
+        assertEquals("Central", result.getAddress().getDistrict());
+        assertEquals("Kameronmouth", result.getAddress().getCity());
+        assertEquals("South Carolina", result.getAddress().getState());
+        assertEquals("United States", result.getAddress().getCountry());
+        assertEquals("997720868", result.getAddress().getPostalCode());
+    }
+
+    @Test
+    void testDelete() {
+        var id = 22L;
+        DJ djPersisted = input.mockDJ(id);
+        when(repository.findById(id)).thenReturn(Optional.of(djPersisted));
+
+        service.delete(id);
+    }
+
+    @Test
+    void testCreateWithNullDj() {
+        Exception exception = assertThrows(RequiredObjectIsNullException.class, () -> {
+            service.create(null);
+        });
+    }
+
+    @Test
+    void testFindByIdWrong() {
+
+        when(repository.findById(123L)).thenThrow(new ResourceNotFoundException(ErrorCode.NOT_FOUND.getMessage()));
+
+        Exception exception = assertThrows(ResourceNotFoundException.class, () -> {
+            service.findById(123L);
+        });
+
+        String expectedMessage = "Not found";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void testUpdateWithNullDj() {
+        Exception exception = assertThrows(RequiredObjectIsNullException.class, () -> {
+            service.update(1L, null);
+        });
+
+        String expectedMessage = "It is not allowed to persist a null object!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 }
